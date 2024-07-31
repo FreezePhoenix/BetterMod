3c2ecaa4cc4a88a6b248eac1bf86d9e1f874f6ae
FreezePhoenix <ariaharper0@gmail.com>
2024-07-31 17:47:07 -0400

    1.7.10: Actually fix the crash with Sodium

--- src/main/java/com/techteam/fabric/bettermod/impl/mixin/MixinSodiumWorldRenderer.java
+++ src/main/java/com/techteam/fabric/bettermod/impl/mixin/MixinSodiumWorldRenderer.java
@@ -21,8 +21,7 @@ public abstract class MixinSodiumWorldRenderer {
 	@WrapWithCondition(
 			at = @At(
 					value = "INVOKE",
-					target = "Lme/jellysquid/mods/sodium/client/render/SodiumWorldRenderer;renderBlockEntity(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/BufferBuilderStorage;Lit/unimi/dsi/fastutil/longs/Long2ObjectMap;FLnet/minecraft/client/render/VertexConsumerProvider$Immediate;DDDLnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;Lnet/minecraft/block/entity/BlockEntity;)V",
-					remap = false
+					target = "Lme/jellysquid/mods/sodium/client/render/SodiumWorldRenderer;renderBlockEntity(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/BufferBuilderStorage;Lit/unimi/dsi/fastutil/longs/Long2ObjectMap;FLnet/minecraft/client/render/VertexConsumerProvider$Immediate;DDDLnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;Lnet/minecraft/block/entity/BlockEntity;)V"
 			),
 			method = "renderBlockEntities(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/BufferBuilderStorage;Lit/unimi/dsi/fastutil/longs/Long2ObjectMap;FLnet/minecraft/client/render/VertexConsumerProvider$Immediate;DDDLnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;)V"
 	)
